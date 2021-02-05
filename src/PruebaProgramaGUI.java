1b140534a48775933da8bd0585c5f7a0ae164d5b
Bryan Valdez <bryan501th@gmail.com>
2021-02-05 15:58:36 -0600

    Se agregaron las cajas de texto de la parte superior derecha con sus respectivos labels

--- src/PruebaProgramaGUI.java
+++ src/PruebaProgramaGUI.java
@@ -28,7 +28,7 @@ class LayoutNulo extends JFrame{
 		add(checkTitle);
 		
 		JTextArea txtbMail = new JTextArea( "Subscribe to our mailing list");
-		txtbMail.setBounds(27, 132, 252, 27);
+		txtbMail.setBounds(27, 132, 225, 27);
 		txtbMail.setLineWrap(true);
 		txtbMail.setWrapStyleWord(true);
 		add(txtbMail);
@@ -63,7 +63,7 @@ class LayoutNulo extends JFrame{
 		add(txtSfw);
 		
 		JTextArea txtbSfw = new JTextArea();
-		txtbSfw.setBounds(27, 377, 252, 27);
+		txtbSfw.setBounds(27, 377, 225, 27);
 		txtbSfw.setLineWrap(true);
 		txtbSfw.setWrapStyleWord(true);
 		add(txtbSfw);
@@ -90,6 +90,44 @@ class LayoutNulo extends JFrame{
 		add(checkMkRw);
 		
 		
+		JLabel txtPreview = new JLabel("Preview");
+		txtPreview.setBounds(277,21,324,13);
+		add(txtPreview);
+		
+		JLabel txtEAddress = new JLabel("Email Address");
+		txtEAddress.setBounds(296,66,136,9);
+		add(txtEAddress);
+		
+		JTextArea txtbEAddress = new JTextArea();
+		txtbEAddress.setBounds(296, 80, 419, 25);
+		txtbEAddress.setLineWrap(true);
+		txtbEAddress.setWrapStyleWord(true);
+		add(txtbEAddress);
+		
+		JLabel txtFName = new JLabel("First Name");
+		txtFName.setBounds(296,118,110,9);
+		add(txtFName);
+		
+		JTextArea txtbFName = new JTextArea();
+		txtbFName.setBounds(296, 132, 419, 25);
+		txtbFName.setLineWrap(true);
+		txtbFName.setWrapStyleWord(true);
+		add(txtbFName);
+		
+		JLabel txtLName = new JLabel("Last Name");
+		txtLName.setBounds(296,170,110,9);
+		add(txtLName);
+		
+		JTextArea txtbLName = new JTextArea();
+		txtbLName.setBounds(296, 184, 419, 25);
+		txtbLName.setLineWrap(true);
+		txtbLName.setWrapStyleWord(true);
+		add(txtbLName);
+		
+		
+		JLabel txtCp = new JLabel("Copy/paste onto your site");
+		txtCp.setBounds(277,300,152,13);
+		add(txtCp);
 		
 		
 		setVisible(true);
