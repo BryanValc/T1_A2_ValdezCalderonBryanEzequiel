317a2b5110d9108801036e0c77efbb169e83bcc2
Bryan Valdez <bryan501th@gmail.com>
2021-02-05 14:23:41 -0600

    se agregaron los demás labels, checkboxes, radiobuttons y cajas de texto de la columna izquierda

--- src/PruebaProgramaGUI.java
+++ src/PruebaProgramaGUI.java
@@ -7,12 +7,13 @@ class LayoutNulo extends JFrame{
 		getContentPane().setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Programa GUI [Layout Nulo]");
-		setSize(771,542);
+		setSize(771,580);
 		setLocationRelativeTo(null);
 		
-		JLabel txtTitulo = new JLabel("<html>The classic Form includes all visible fields for \n this list.<html>");
+		JLabel txtTitulo = new JLabel("<html>The classic Form includes all visible fields for this list.<html>");
 		txtTitulo.setBounds(27,21,230,25);
 		add(txtTitulo);
+		
 		
 		JLabel txtFormOptions = new JLabel("Form options");
 		txtFormOptions.setBounds(27,70,77,13);
@@ -32,16 +33,66 @@ class LayoutNulo extends JFrame{
 		txtbMail.setWrapStyleWord(true);
 		add(txtbMail);
 		
-		/*JLabel txtNumControl = new JLabel("Numero de control:");
-		txtNumControl.setBounds(210, 55,50,50);
-		add(txtNumControl);
 		
-		JTextField cajNumControl = new JTextField(10);
-		cajNumControl.setBounds(170,55,50,50);
-		add(cajNumControl);*/
+		ButtonGroup bg = new ButtonGroup();
+		JRadioButton radioOrf = new JRadioButton("only required fields");
+		bg.add(radioOrf);
+		radioOrf.setBounds(27, 195, 148, 20);
+		add(radioOrf);
+		JRadioButton radioAll = new JRadioButton("all fields");
+		bg.add(radioAll);
+		radioAll.setBounds(27, 217, 95, 20);
+		add(radioAll);
+		
+		JLabel txtEditFields = new JLabel("(edit required fields in the form builder)");
+		txtEditFields.setBounds(42,245,220,13);
+		add(txtEditFields);
+		
+		
+		JCheckBox checkIgf = new JCheckBox("interest group fields");
+		checkIgf.setBounds(25, 294, 151, 20);
+		add(checkIgf);
+		
+		JCheckBox checkRfi = new JCheckBox("required field indicators");
+		checkRfi.setBounds(25, 318, 170, 20);
+		add(checkRfi);
+		
+		
+		JLabel txtSfw = new JLabel("Set form width");
+		txtSfw.setBounds(27,360,96,13);
+		add(txtSfw);
+		
+		JTextArea txtbSfw = new JTextArea();
+		txtbSfw.setBounds(27, 377, 252, 27);
+		txtbSfw.setLineWrap(true);
+		txtbSfw.setWrapStyleWord(true);
+		add(txtbSfw);
+		
+		
+		JLabel txtEyf = new JLabel("Enhance your form");
+		txtEyf.setBounds(27,423,120,13);
+		add(txtEyf);
+		
+		JCheckBox checEvPop= new JCheckBox("enable evil popup mode");
+		checEvPop.setBounds(25, 440, 173, 20);
+		add(checEvPop);
+		
+		JCheckBox checkJS = new JCheckBox("disable all JavaScript");
+		checkJS.setBounds(25, 462, 163, 20);
+		add(checkJS);
+		
+		JCheckBox checkfLink = new JCheckBox("include archive link");
+		checkfLink.setBounds(25, 484, 150, 20);
+		add(checkfLink);
+		
+		JCheckBox checkMkRw = new JCheckBox("include Monkey Rewards link");
+		checkMkRw.setBounds(25, 506, 196, 20);
+		add(checkMkRw);
+		
+		
+		
 		
 		setVisible(true);
-		
 		
 		
 	}
